5fb791221dba131fd930cb7e2dbc55ca63ee72b9
pranitha-1501 <pranitha.dondapati@gmail.com>
2025-06-06 22:16:43 -0600

    added the screenshot in TestUtils,java

--- src/main/java/com/qa/testutils/TestUtil.java
+++ src/main/java/com/qa/testutils/TestUtil.java
@@ -1,12 +1,15 @@
 package com.qa.testutils;
 
 import com.qa.base.TestBase;
+import org.apache.commons.io.FileUtils;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.ss.usermodel.WorkbookFactory;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 
 
-
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -43,5 +46,11 @@ public class TestUtil extends TestBase {
             }
         }
         return data;
+    }
+
+    public static void takeScreenshotAtEndOfTest() throws IOException {
+        File scrFile = ((TakesScreenshot) dr).getScreenshotAs(OutputType.FILE);
+        String currentDir = System.getProperty("user.dir");
+        FileUtils.copyFile(scrFile, new File(currentDir + "/screenshots/" + System.currentTimeMillis() + ".png"));
     }
 }
